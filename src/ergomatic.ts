dc875f20ac180f043ca14d6f7f9e91f2f7633552
ross-weir <29697678+ross-weir@users.noreply.github.com>
2023-08-11 09:05:54 +1000

    add compnent management to ergomatic

--- src/ergomatic.ts
+++ src/ergomatic.ts
@@ -2,28 +2,80 @@ import { PluginManager } from "./plugins/plugin_manager.ts";
 import { ErgomaticConfig } from "./config.ts";
 import { Component } from "./component.ts";
 
-export class Ergomatic extends Component {
-  private readonly pluginManager: PluginManager;
+interface ErgomaticEvent {
+  "component:error": CustomEvent<{ component: Component; error: Error }>;
+}
 
-  constructor(config: ErgomaticConfig) {
-    super(config);
+interface ErgomaticOpts {
+  config: ErgomaticConfig;
+  pluginManager?: PluginManager;
+}
 
-    this.pluginManager = new PluginManager(config);
-  }
+export class Ergomatic extends Component<ErgomaticEvent> {
+  #isRunning = false;
+  readonly #components: Component[];
 
-  name(): string {
-    return "Ergomatic";
+  constructor(opts: ErgomaticOpts) {
+    super(opts.config, "Ergomatic");
+
+    const pluginManager = opts.pluginManager ?? new PluginManager(opts.config);
+
+    this.#components = [pluginManager];
   }
 
   public async start(): Promise<void> {
+    if (this.#isRunning) {
+      this.logger.warning("Ergomatic is already running");
+
+      return;
+    }
+
     this.logger.debug("Starting Ergomatic");
 
-    await this.pluginManager.start();
+    const promises = this.#components.map(async (component) => {
+      try {
+        this.logger.debug(`Starting component ${component.name}`);
+
+        await component.start();
+      } catch (e) {
+        this.#onComponentError(component, e);
+      }
+    });
+
+    await Promise.allSettled(promises);
+
+    this.#isRunning = true;
   }
 
   public async stop(): Promise<void> {
+    if (!this.#isRunning) {
+      this.logger.warning("Ergomatic is not running");
+
+      return;
+    }
+
     this.logger.debug("Stopping Ergomatic");
 
-    await this.pluginManager.stop();
+    const promises = this.#components.map(async (component) => {
+      try {
+        this.logger.debug(`Stopping component ${component.name}`);
+
+        await component.stop();
+      } catch (e) {
+        this.#onComponentError(component, e);
+      }
+    });
+
+    await Promise.allSettled(promises);
+
+    this.#isRunning = false;
+  }
+
+  #onComponentError(component: Component, error: Error) {
+    this.logger.debug(`Component ${component.name} failed: ${error}`);
+
+    this.dispatchEvent(
+      new CustomEvent("component:error", { detail: { component, error } }),
+    );
   }
 }
--- src/plugins/plugin_manager.ts
+++ src/plugins/plugin_manager.ts
@@ -32,7 +32,7 @@ export class PluginManager extends Component<PluginManagerEvent> {
   #_plugins: ManagedPlugin[];
 
   constructor(config: ErgomaticConfig, pluginCtorMap = pluginConstructorMap) {
-    super(config);
+    super(config, "PluginManager");
 
     this.#pluginConstructorMap = pluginCtorMap;
     this.#_plugins = config.plugins.filter((p) => p.enabled).map((
@@ -41,10 +41,6 @@ export class PluginManager extends Component<PluginManagerEvent> {
       plugin: this.#createPlugin(config, pluginEntry),
       state: PluginState.Stopped,
     }));
-  }
-
-  name(): string {
-    return "PluginManager";
   }
 
   public async start(): Promise<void> {
